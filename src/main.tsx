cbc55d95b31979a8729261fd026f4d14744b7c32
mecsbalint <101649625+mecsbalint@users.noreply.github.com>
2025-09-13 11:45:19 +0200

    Implement ErrorContext

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import './main.css'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/router'
 import { AuthContextProvider } from './contexts/AuthContext'
+import { ErrorContextProvider } from './contexts/ErrorContext'
 
 createRoot(document.getElementById('root') as HTMLElement).render(
   <StrictMode>
-    <AuthContextProvider>
-      <RouterProvider router={router} />
-    </AuthContextProvider>
+    <ErrorContextProvider>
+      <AuthContextProvider>
+        <RouterProvider router={router} />
+      </AuthContextProvider>
+    </ErrorContextProvider>
   </StrictMode>
 )
